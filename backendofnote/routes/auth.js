7536b86e6d820a3a3e8ef33cae84373aec863962
pranti <prantibanda@gmail.com>
2024-01-23 16:07:45 +0530

    login done

--- backendofnote/routes/auth.js
+++ backendofnote/routes/auth.js
@@ -45,8 +45,43 @@ async (req, res)=>{
   }
   catch(error){
     console.log(error.message);
-    res.status(500).send("Some error occured");
+    res.status(500).send("Internal server error");
   }
 })
+
+//Authenticate user using : POST "/api/auth/login".
+authRouter.post("/login",[
+  body('email', "Enter valid email").isEmail(),
+  body('password', "Password cannot be blank").exists(),
+], 
+async (req, res)=>{
+  //if any error return bad request and errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const {email, password} = req.body;
+  try {
+    let user = await UserModel.findOne({email});
+    if(!user){
+      return res.status(400).json({ errors: "Please try to login with correct credentials" });
+    }
+    const passwordCompare = await bcrypt.compare(password, user.password)
+    if(!passwordCompare){
+      return res.status(400).json({ errors: "Please try to login with correct credentials" });
+    }
+    const data = {
+      user:{
+        id: user.id
+      }
+    }
+    const authToken = jwt.sign(data, JWT_SECRET)
+    res.json({authToken});
+
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal server error");
+  }
+});
 
 export default authRouter;
